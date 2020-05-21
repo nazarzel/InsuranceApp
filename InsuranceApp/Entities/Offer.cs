4c89d76430d96f0bb3e670f144a8e6301f37aefc
Nazar Svyryd <nazar.svyryd@gmail.com>
2020-05-21 20:25:53 +0200

    Added counter

--- InsuranceApp/Entities/Offer.cs
+++ InsuranceApp/Entities/Offer.cs
@@ -12,5 +12,10 @@ namespace InsuranceApp.Entities
         public int numOfDays { get; set; }
         public CarType CarType { get; set; }
         public decimal Price { get; set; }
+
+        public string CarouselImageUrl { get; set; }
+        public string CarouselName { get; set; }
+        public string CarouselDescription {get;set;}
+        public string ProductCardImageUrl { get; set; }
     }
 }
