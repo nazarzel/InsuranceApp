3f36c88f95a46f40eae3f8f8c2f5eef7176a41f1
toth akosh <ti1lolka@gmail.com>
2020-05-23 19:37:19 +0200

    Controller

---
+++ InsuranceApp/Controllers/InsuranceController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsuranceApp.Entities;
+using InsuranceApp.Repositories;
+using InsuranceApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceApp.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public InsuranceController(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            return await Task.Run(()=>View());
+        }
+        public async Task<IActionResult> InsuranceInformationForCarAsync([FromForm]OrderInsuranceViewModel orderData)
+        {
+            if(ModelState.IsValid)
+            {
+                var orderToCreate = new Order()
+                {
+                    Id = Guid.NewGuid(),
+                    Car = orderData.CarForInsurance,
+                    DayStart = DateTime.Today,
+                    PaymentType = orderData.PaymentType,
+                    Price = 119.99M,
+                    ReminderSent = false,
+                    PdfSent = false,
+                };
+                orderRepository.CreateNewOrderAsync(orderToCreate);
+                return await Task.Run(() => RedirectToAction("PaymentForInsuranceAsync"));
+            }
+            return await Task.Run(() => View(orderData));
+        }
+
+    }
+}
---
+++ InsuranceApp/ViewModels/OrderInsuranceViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InsuranceApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceApp.ViewModels
+{
+    public class OrderInsuranceViewModel
+    {
+        public Car CarForInsurance { get; set; }
+        public PaymentType PaymentType { get; set; }
+    }
+}
